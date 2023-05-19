4c4f826650f2e764616ef65cd3490c7ac17c27a2
taljbear <72319147+taljbear@users.noreply.github.com>
2023-05-19 12:21:48 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onPinPressed(TouchPin.P0, function () {
+    for (let index = 0; index < 4; index++) {
+    	
+    }
+    basic.showString("Love Meter")
+    basic.showNumber(randint(0, 100))
+})
